0701c160652776ce4011dc6cc79cb0bb1bdd683c
Mano Raichon <mano.raichon@orange.fr>
2023-04-15 19:39:47 +0200

    My cart (sidebar)

--- resources/views/client_layout/header.blade.php
+++ resources/views/client_layout/header.blade.php
@@ -109,24 +109,19 @@
             <a href="#" class="close-side"><i class="fa fa-times"></i></a>
             <li class="cart-box">
                 <ul class="cart-list">
-                    <li>
-                        <a href="#" class="photo"><img src="frontend/images/img-pro-01.jpg" class="cart-thumb" alt="" /></a>
-                        <h6><a href="#">Delica omtantur </a></h6>
-                        <p>1x - <span class="price">$80.00</span></p>
-                    </li>
-                    <li>
-                        <a href="#" class="photo"><img src="frontend/images/img-pro-02.jpg" class="cart-thumb" alt="" /></a>
-                        <h6><a href="#">Omnes ocurreret</a></h6>
-                        <p>1x - <span class="price">$60.00</span></p>
-                    </li>
-                    <li>
-                        <a href="#" class="photo"><img src="frontend/images/img-pro-03.jpg" class="cart-thumb" alt="" /></a>
-                        <h6><a href="#">Agam facilisis</a></h6>
-                        <p>1x - <span class="price">$40.00</span></p>
-                    </li>
+                    @if(Session::has('topCart'))
+                        @foreach(Session::get('topCart') as $item)
+                            <li>
+                                <a href="#" class="photo"><img src="{{ asset('storage/product_images/' . $item['product_image']) }}" class="cart-thumb" alt="" /></a>
+                                <h6><a href="#">{{ $item['product_name'] }}</a></h6>
+                                <p>{{ $item["qty"] }}x - <span class="price">${{ $item['product_price'] }}</span></p>
+                            </li>
+                        @endforeach
+                    @endif
+
                     <li class="total">
                         <a href="{{ route('client.cart.index') }}" class="btn btn-default hvr-hover btn-cart">VIEW CART</a>
-                        <span class="float-right"><strong>Total</strong>: $180.00</span>
+                        <span class="float-right"><strong>Total</strong>: ${{ Session::get('cart')->totalPrice }}</span>
                     </li>
                 </ul>
             </li>
