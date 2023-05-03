ea862d4b3293b8a1e0b1bd184155708df1f92d1a
Mano Raichon <mano.raichon@orange.fr>
2023-05-03 16:43:03 +0200

    Fixed-top Search header

--- resources/views/client_layout/header.blade.php
+++ resources/views/client_layout/header.blade.php
@@ -139,20 +139,21 @@
         <!-- End Side Menu -->
     </nav>
     <!-- End Navigation -->
+    <!-- Start Top Search -->
+    <form action="{{ route('client.search') }}" method="POST">
+        @csrf
+        <div class="top-search">
+            <div class="container">
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                    <input type="text" class="form-control" placeholder="Search" name="search">
+                    <span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
+                </div>
+            </div>
+        </div>
+    </form>
+    <!-- End Top Search -->
 </header>
 <!-- End Main Top -->
 
-<!-- Start Top Search -->
-<form action="{{ route('client.search') }}" method="POST">
-    @csrf
-    <div class="top-search">
-        <div class="container">
-            <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                <input type="text" class="form-control" placeholder="Search" name="search">
-                <span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
-            </div>
-        </div>
-    </div>
-</form>
-<!-- End Top Search -->
+
